e5be4843df6eb1091aa26614f6a8d6f8b7228aa0
Joe Liberi <m4punk@gmail.com>
2019-07-26 12:04:18 -0400

    Moved the adding the video element to the bottom of the index file for testing

--- js/index.js
+++ js/index.js
@@ -176,14 +176,6 @@ $(document).ready(function()
         }
     }
 
-    /*
-    Item tile and phadia prime tile functionality. When one is clicked we add a html5 video 
-    to the screen.
-    */
-    $('.itemTile, .primeItemTile').on('click', function(){
-        $('#overlay-frame').append("<Video id='tileVideo' controls autoplay onended='videoEnded()' onpause='videoPause()'><source src='"+ $(this).data('video-url') +"'></Video>")
-    });
-
     // Phadia prime close button
     $('#phadiaprimeTile').on('click', function(){
         $('#splash-content').hide();
