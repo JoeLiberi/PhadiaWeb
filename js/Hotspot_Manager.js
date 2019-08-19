048e4f1c3e8d88937be23a09ae82726a8f0fb22c
Joe Liberi <m4punk@gmail.com>
2019-08-19 19:02:49 -0400

    Cropped 250 images. Added hotspots to 250.

--- js/Hotspot_Manager.js
+++ js/Hotspot_Manager.js
@@ -751,7 +751,7 @@ function Hotspot_Manager(jsonFilePath, sysSelect, videoId, imgPath, videoPath, b
 
     function hsClicked(event){
         // get the number of the button from its id
-        clearTimers();
+        // clearTimers();
         var butId = event.target.id.split("_");
         butId = parseInt(butId[1]);
 
@@ -948,7 +948,7 @@ function Hotspot_Manager(jsonFilePath, sysSelect, videoId, imgPath, videoPath, b
         relPos = 1.0 -  Math.max(Math.min(relPos / vidContSize[0], 0.92), 0.0);
         // relPos = 1.0 -  Math.min(relPos / vidContSize[0], 1.0);
         lastRelPos = 1.0 - relPos;
-        console.log('lastRelPos' + lastRelPos)
+        // console.log('lastRelPos' + lastRelPos)
 
         calcHotSpotPos3D(lastRelPos, hideId);
 
